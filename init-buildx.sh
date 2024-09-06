123b12047cd197dbb7dc1c4805fbc9cf2f2a6e77
satanTime <m@sudo.eu>
2024-09-06 12:24:43 +0200

    fix: refreshing qemu to fix the isuse with libc-bin

--- init-buildx.sh
+++ init-buildx.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# https://stuffivelearned.org/doku.php?id=misc:docker_x_compile_crash
+docker buildx rm puppeteer-node && \
+docker run --privileged --rm tonistiigi/binfmt --uninstall 'qemu-*' && \
+docker run --privileged --rm tonistiigi/binfmt --install all && \
 docker buildx create --use --bootstrap \
   --name puppeteer-node \
   --driver docker-container \
