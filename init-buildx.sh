52150936cd9d13afd33719fca1dfef2a5b1cb026
satanTime <m@sudo.eu>
2026-03-12 06:24:09 +0100

    build(buildx): removing breaking commands

--- init-buildx.sh
+++ init-buildx.sh
@@ -3,10 +3,13 @@
 # https://stuffivelearned.org/doku.php?id=misc:docker_x_compile_crash
 # https://github.com/docker/buildx/issues/1170
 # Errors were encountered while processing: libc-bin
+
+# Broke Mac Docker Desktop
+#docker run --privileged --rm tonistiigi/binfmt --uninstall 'qemu-*' && \
+#docker run --privileged --rm tonistiigi/binfmt --install all && \
+#docker run --privileged --rm multiarch/qemu-user-static --reset -p yes -c yes && \
+
 docker buildx rm puppeteer-node && \
-docker run --privileged --rm tonistiigi/binfmt --uninstall 'qemu-*' && \
-docker run --privileged --rm tonistiigi/binfmt --install all && \
-docker run --privileged --rm multiarch/qemu-user-static --reset -p yes -c yes && \
 docker buildx create --use --bootstrap \
   --name puppeteer-node \
   --driver docker-container \
